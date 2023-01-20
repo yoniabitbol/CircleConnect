4bb8736f775f21ceefdececad7bffbd2f8711900
William Wells <williamwells2013@hotmail.com>
2023-01-19 22:10:12 -0500

    Fix minor linting errors

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -4,10 +4,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('', () => {
-    test('Example render test', () => {
-        render(<App / >,);
-        expect(
-            screen.getByText(/App/)
-        ).toBeInTheDocument();
-    });
+  test('Example render test', () => {
+    render(<App />);
+    expect(
+      screen.getByText(/App/),
+    ).toBeInTheDocument();
+  });
 });
