d41d2b454e0d4cd463a3953e065639eb23cd77d1
William Wells <williamwells2013@hotmail.com>
2023-01-19 22:07:46 -0500

    Add example client test

---
+++ client/src/App.test.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('', () => {
+    test('Example render test', () => {
+        render(<App / >,);
+        expect(
+            screen.getByText(/App/)
+        ).toBeInTheDocument();
+    });
+});
