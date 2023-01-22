d35f04a2c988b1233058621f562abd627c7ae14f
Reuven1203 <reuven1203@gmail.com>
2023-01-21 19:43:10 -0500

    Implemented Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './Routes/SignUp';
+import Login from './Routes/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   );
 }
