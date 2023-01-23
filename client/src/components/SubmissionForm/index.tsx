0993bd8dfa878ea50d295640fa1fcc9e35cc51d5
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-01-23 01:33:56 -0500

    cleaned up code

--- client/src/components/SubmissionForm/index.tsx
+++ client/src/components/SubmissionForm/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, FormikHelpers} from "formik";
 import inputFieldModel from "../../Models/InputFieldModel";
 import InputField from "./InputField";
 
 import ThirdPartyLogin from "./ThirdPartyLogin";
+
+// this needs to be dynamic or imported from a file
+interface Values {
+  fname: string;
+  lname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const SubmissionForm: React.FC<{
   fields: inputFieldModel[];
@@ -11,41 +20,55 @@ const SubmissionForm: React.FC<{
   buttonField: string;
 }> = (props) => {
   const { fields, header, buttonField } = props;
-  const onSubmitHandler = () => {
-    console.log("test");
-  };
+  const onSubmitHandler = () => {(
+    values: Values,
+    { setSubmitting }: FormikHelpers<Values>
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  }}
+
   return (
-    <div className="">
-      <h1 className="text-3xl font-medium">{header}</h1>
-      <Formik
-        onSubmit={onSubmitHandler}
-        initialValues={{
-          fname: "",
-          lname: "",
-          email: "",
-          password: "",
-          confirmPassword: "",
-        }}
-      >
-        <Form className=" ">
-          {fields.map((field) => (
-            <InputField
-              key={field.id}
-              type={field.type}
-              name={field.name}
-              placeholder={field.placeholder}
-              Error={field.Error}
-            />
-          ))}
-          <button
-            type="submit"
-            className="block mt-4 w-full px-2 py-3 rounded-lg bg-signup-button text-white hover:bg-signup-button-hover shadow-xl shadow-placeholder-purple"
-          >
-            {buttonField}
-          </button>
-        </Form>
-      </Formik>
-      <ThirdPartyLogin />
+    <div className="lg:w-1/2 lg:mt-0 mt-20">
+      <div className="lg:w-1/2 w-2/3 mx-auto">
+        <h1 className="text-3xl font-medium">{header}</h1>
+        <Formik
+          onSubmit={onSubmitHandler}
+          
+          // Frontend validation needs to be added to lib folder and imported here
+
+          // These also need to be dynamic
+          initialValues={{
+            fname: "",
+            lname: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          }}
+        >
+          <Form className=" ">
+            {fields.map((field) => (
+              <InputField
+                key={field.id}
+                type={field.type}
+                name={field.name}
+                placeholder={field.placeholder}
+                Error={field.Error}
+              />
+            ))}
+            <button
+              type="submit"
+              className="block mt-4 w-full px-2 py-3 rounded-lg bg-signup-button
+               text-white hover:bg-signup-button-hover shadow-xl shadow-placeholder-purple"
+            >
+              {buttonField}
+            </button>
+          </Form>
+        </Formik>
+        <ThirdPartyLogin />
+      </div>
     </div>
   );
 };
