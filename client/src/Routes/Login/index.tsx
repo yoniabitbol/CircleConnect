602ddc7387d5e2ef657e1dbc354fe2f4dc7acd57
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-01-23 20:48:25 -0500

    linked firebase auth to login page

--- client/src/Routes/Login/index.tsx
+++ client/src/Routes/Login/index.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import AuthContent from "../../components/AuthContent";
 import SubmissionForm from "../../components/SubmissionForm";
 import { LoginFields } from "../../lib/InputFieldModels";
-// import useSignup from '../../hooks/useSignup';
+import useLogin from '../../hooks/useLogin';
 
 
 const Login: React.FC = () => {
-    const onSubmitHandler = (values: any, actions: any) => {
-        console.log(values, actions);
+  const {error, login} = useLogin();
+
+    const onSubmitHandler = (values: any) => {
+      login(values.email, values.password)
     }
   return (
     <div className="lg:flex justify-center lg:text-left text-center">
       <AuthContent />
-      <SubmissionForm error={'Hey fake error'} onSubmit={onSubmitHandler} header="Login" fields={LoginFields} buttonField="Login" />
+      <SubmissionForm 
+        error={error} 
+        onSubmit={onSubmitHandler} 
+        header="Login" 
+        fields={LoginFields} 
+        buttonField="Login" 
+      />
       <div className="hidden lg:block lg:absolute lg:left-1/3 lg:bottom-40">
         <img className="h-96" src="Mascot/img.png" alt="logo" />
       </div>
