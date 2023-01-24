af1bd097005d0f7dbea9e8bf78b8d1c6cced9827
William Wells <williamwells2013@hotmail.com>
2023-01-23 23:39:00 -0500

    Mock user auth in client side tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,11 +3,22 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('./firebase/config', () => ({
+  auth: () => 'test',
+}));
+
+jest.mock('./hooks/useAuthContext', () => ({
+  __esModule: true,
+  default: () => {
+    return {user: null, authIsReady: true};
+  },
+}));
+
 describe('Get title on login Screen test', () => {
   test('', () => {
     render(<App />);
     expect(
-      screen.getByText(/CircleConnect/),
+        screen.getByText('CircleConnect'),
     ).toBeInTheDocument();
   });
 });
