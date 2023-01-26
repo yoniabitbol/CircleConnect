708212a8aa7eca651eae4a25df71ee8f748a68ee
William Wells <williamwells2013@hotmail.com>
2023-01-26 02:51:34 -0500

    Most of tests necessary for full backend coverage

--- server/app.test.ts
+++ server/app.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import app from './app';
+import User from "./models/userModel";
+const mockingoose = require('mockingoose');
 
 jest.mock('./usingAuth', () => ({
     default: false,
@@ -10,10 +12,74 @@ describe('Request default route', () => {
   test('', (done) => {
     request(app)
       .get('/')
-      .set({ Authorization: "token: test" })
       .then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
   });
 });
+
+describe('Request invalid route', () => {
+    test('', (done) => {
+        request(app)
+            .get('/test')
+            .then((response) => {
+                expect(response.statusCode).toBe(400);
+                done();
+            });
+    });
+});
+
+describe('Request get all users', () => {
+    test('', (done) => {
+        request(app)
+            .get('/api/users')
+            .then((response) => {
+                expect(response.statusCode).toBe(500);
+                expect(response.text).toBe('{"status":"error","message":"Get all users not implemented yet"}');
+                done();
+            });
+    });
+});
+
+describe('Request create user', () => {
+    test('', (done) => {
+        mockingoose(User).toReturn({ user_id: "testUser", name: "testUser", email: "testUser" },
+            'findOne');
+
+        request(app)
+            .post('/api/users')
+            .send({"user_id": "test"})
+            .then((response) => {
+                expect(response.statusCode).toBe(201);
+                expect(response.text).toContain('{"status":"user exists","data":{"user":{"user_id":' +
+                    '"testUser","name":"testUser","email":"testuser","_id":');
+                done();
+            });
+    });
+});
+
+// describe('Request create user no user specified error', () => {
+//     test('', (done) => {
+//         request(app)
+//             .post('/api/users')
+//             .then((response) => {
+//                 expect(response.statusCode).toBe(400);
+//                 expect(response.text).toBe('{"status":"ERROR: TypeError: Cannot read properties of ' +
+//                     'undefined (reading \'user_id\')","message":"error adding user"}');
+//                 done();
+//             });
+//     });
+// });
+
+//describe('Request get specific users', () => {
+//    test('', (done) => {
+//        request(app)
+//            .get('/api/users/test')
+//            .then((response) => {
+//                expect(response.statusCode).toBe(500);
+//                expect(response.text).toBe('{"status":"error","message":"Get user not implemented yet"}');
+//                done();
+//            });
+//    });
+//});
