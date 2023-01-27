a45c6093957ef362431d2850dd86e49a031c90ce
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-01-27 01:20:27 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,47 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./Routes/SignUp";
 import Login from "./Routes/Login";
-import Profile from "./Routes/Profile";
 import useAuthContext from "./hooks/useAuthContext";
 import AuthHeader from "./components/AuthHeader";
 import ForgotPass from "./Routes/ForgotPass";
 import useLogout from "./hooks/useLogout";
 
 function App() {
-    const { user, authIsReady } = useAuthContext();
-    const { logout } = useLogout();
+  const { user, authIsReady } = useAuthContext();
+  const { logout } = useLogout();
   return (
-      <>
-          {authIsReady && (
-              <BrowserRouter>
-                  <Routes>
-                    <Route path="/" element={user ? <button onClick={logout}> Logout </button> : <Navigate to='/login' />} />
-                    <Route path="/*" element={<AuthHeader />} >
-                      <Route path="signup" element={!user ? <SignUp /> : <Navigate to='/' />} />
-                      <Route path="login" element={!user ? <Login /> : <Navigate to='/' />} />
-                      <Route path="forgot" element={!user ? <ForgotPass /> : <Navigate to='/' />} />
-                    </Route>
-                  </Routes>
-              </BrowserRouter>)
-          }
-      </>
+    <>
+      {authIsReady && (
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                user ? (
+                  <button onClick={logout}> Logout </button>
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+            />
+            <Route path="/*" element={<AuthHeader />}>
+              <Route
+                path="signup"
+                element={!user ? <SignUp /> : <Navigate to="/" />}
+              />
+              <Route
+                path="login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="forgot"
+                element={!user ? <ForgotPass /> : <Navigate to="/" />}
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
