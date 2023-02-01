927f40d15de7a2c0ba4474c892e1763b4c285c5a
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-01-31 22:37:11 -0500

    updated the navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,86 +1,246 @@
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable max-len */
 import React from "react";
-import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import RssFeedIcon from '@mui/icons-material/RssFeed';
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 const NavBar: React.FC = () => {
   return (
-    <>
-      {/* <nav classNameName="h-32">
-        <div classNameName="container flex flex-wrap items-center  mx-auto">
-            <Link to='/'>
-          <img
-            classNameName="h-20"
-            src="Brand Logo/officccccc.jpg"
-            alt="logo"
-          />
-        </Link>
-        <Link to='/feed'>
-        <RssFeedIcon classNameName="h-20" />
-        </Link>
-        <Link to='/network'>
-        <PeopleAltOutlinedIcon classNameName="h-20" />
-        </Link>
-        <Link to='/jobs'>
-        <WorkOutlineOutlinedIcon classNameName="h-20" />
-        </Link>
-        <Link to='/chat'>
-        <ChatBubbleOutlineOutlinedIcon classNameName="h-20" />
-        </Link>
-        <Link to='/notifications'>
-        <NotificationsNoneOutlinedIcon classNameName="h-20" />
-        </Link>
-        </div>
-       
-      </nav> */}
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="Brand Logo/officccccc.jpg" alt="" />
+          </a>
+        </Logo>
+        <Nav>
+          <NavListWrap>
+            <NavList className="active">
+              <a>
+                <RssFeedIcon sx={{color:"#4D47C3"}}/>
+                <span>Feed</span>
+              </a>
+            </NavList>
 
-<nav className="bg-white px-2 sm:px-4 py-2.5 light:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 light:border-gray-600">
-  <div className="container flex flex-wrap items-center justify-between mx-auto">
-  <a href="" className="flex items-center">
-      <img src="Brand Logo/officccccc.jpg" className="h-20" alt="Logo" />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-  </a>
-  <div className="flex md:order-2">
-      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-  </div>
-  <div className="hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-      <li>
-      <RssFeedIcon className="h-20" />
-        <a href="/feed" className="block py-2 pl-3 pr-4 text-black bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Feed</a>
-      </li>
-      <li>
-        <PeopleAltOutlinedIcon className="h-20" />
-        <a href="/network" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Network</a>
-      </li>
-      <li>
-        <WorkOutlineOutlinedIcon className="h-20" />
-        <a href="/jobs" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Jobs</a>
-      </li>
-      <li>
-        <ChatBubbleOutlineOutlinedIcon className="h-20" />
-        <a href="chat" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Chat</a>
-      </li>
-      <li>
-        <NotificationsNoneOutlinedIcon className="h-20" />
-        <a href="notifications" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Notifications</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+            <NavList>
+              <a>
+                <PeopleAltOutlinedIcon sx={{color:"#4D47C3"}}/>
+                <span>Network</span>
+              </a>
+            </NavList>
 
-      <Outlet />
-    </>
+            <NavList>
+              <a>
+                <WorkOutlineOutlinedIcon sx={{color:"#4D47C3"}}/>
+                <span>Jobs</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <ChatBubbleOutlineOutlinedIcon sx={{color:"#4D47C3"}}/>
+                <span>Chat</span>
+              </a>
+            </NavList>
+
+            <NavList>
+              <a>
+                <NotificationsNoneOutlinedIcon sx={{color:"#4D47C3"}}/>
+                <span>Notifications</span>
+              </a>
+            </NavList>
+
+            <Search>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+          <Icon>
+            <SearchIcon sx={{color:"#4D47C3"}}/>
+          </Icon>
+        </Search>
+            <User>
+              <a>
+                <img src="/images/user.svg" alt="" />
+                <span>Me</span>
+                <img src="/images/down-icon.svg" alt="" />
+              </a>
+
+              <SignOut>
+                <a>Sign Out</a>
+              </SignOut>
+            </User>
+          </NavListWrap>
+        </Nav>
+      </Content>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0px;
+  width: 93px;
+`;
+
+const Search = styled.div`
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 280px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 218px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
+    }
+  }
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  position: absolute;
+  z-index: 1;
+  top: 7px;
+  left: 2px;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  list-style-type: none;
+  .active {
+    span:after {
+      content: "";
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2s ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
+`;
+
+const NavList = styled.li`
+  display: flex;
+  align-items: center;
+  a {
+    align-items: center;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 52px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+    }
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+  }
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
+
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`;
+
+const User = styled(NavList)`
+  a > svg {
+    width: 24px;
+    border-radius: 50%;
+  }
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
 
 export default NavBar;
