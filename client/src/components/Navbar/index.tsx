ac36e37202455b0894aa9275530bbe4d515295ee
Reuven1203 <reuven1203@gmail.com>
2023-02-02 16:49:37 -0500

    Responsive

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import NavLinks from "./NavLinks";
-// import styled from "styled-components";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar";
 import { Avatar, Button } from "@mui/material";
-// import RssFeedIcon from '@mui/icons-material/RssFeed';
-// import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-// import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
-// import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
-// import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-// import SearchIcon from '@mui/icons-material/Search';
+import useLogout from "../../hooks/useLogout";
 
 const NavBar: React.FC = () => {
+  const {logout} = useLogout();
   return (
     <div className="max-w-full p-2 flex items-center">
       <div className="flex w-1/2 h-max  max-md:w-4/5 ">
@@ -24,15 +19,18 @@ const NavBar: React.FC = () => {
         </Link>
         <NavLinks />
       </div>
-      <div className=" flex  items-center justify-start p-2.5 space-x-3.5">
-        <div className="">
-          <SearchBar/>
+      <div className="flex justify-end items-center w-1/2">
+        <div className="flex items-center p-2.5">
+          <div className="max-md:hidden">
+            <SearchBar/>
+          </div>
+        </div>
+        <div className="flex space-x-7">
+          <Avatar />
+          <Button onClick={logout} variant="outlined">Logout</Button>
         </div>
       </div>
-      <div className="flex space-x-7 max-md:hidden">
-        <Avatar />
-        <Button variant="outlined">Logout</Button>
-      </div>
+      
     </div>
     )};
 
--- client/src/components/SearchBar/index.tsx
+++ client/src/components/SearchBar/index.tsx
@@ -4,15 +4,15 @@ import {  Search } from "@mui/icons-material";
 const SearchBar = () => {
   return (
     <TextField className="max-md:hidden"
-      id="input-with-icon-textfield"
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Search sx={{color:"#4B47B6"}}/>
-          </InputAdornment>
-        ),
-      }}
-      placeholder="Search"
+               id="input-with-icon-textfield"
+               InputProps={{
+                 startAdornment: (
+                   <InputAdornment position="start">
+                     <Search sx={{color:"#4B47B6"}}/>
+                   </InputAdornment>
+                 ),
+               }}
+               placeholder="Search"
     />
   );
 };
