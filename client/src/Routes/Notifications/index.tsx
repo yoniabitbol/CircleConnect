e420b54450e8c82cceac9b7509d9c2b82fda8d59
lucadallaire <luca.dallaire@gmail.com>
2023-02-04 10:49:38 -0500

    Began setting up files for front-end notifications page

---
+++ client/src/Routes/Notifications/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserNotifications from "../../components/UserNotifications";
+
+const Notifications: React.FC = () => {
+  return <UserNotifications />;
+};
+
+export default Notifications;
