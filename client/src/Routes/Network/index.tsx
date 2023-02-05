8491e32aacb62eeb66e65ab7c2da85161b8406aa
Ankita Ingle <aningle31@gmail.com>
2023-02-04 21:46:27 -0500

    Comment

--- client/src/Routes/Network/index.tsx
+++ client/src/Routes/Network/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Network: React.FC = () => {
   return (
     <div className="lg:flex justify-center lg:text-left">
-      <div className="grid grid-cols-2 p-8 w-3/5">
+      {/* <div className="grid grid-cols-2 p-8 w-3/5">
         <div className="w-4/5">357 CONNECTIONS</div>
         <div>Search</div>
-      </div>
+      </div> */}
     </div>
   );
 };
