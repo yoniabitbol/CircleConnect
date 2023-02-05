2c60cf2adafd5f389412fcd85d27ac4d90469d36
Ankita Ingle <aningle31@gmail.com>
2023-02-04 22:03:00 -0500

    Add styling to first row

--- client/src/Routes/Network/index.tsx
+++ client/src/Routes/Network/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Network: React.FC = () => {
   return (
-    <div className="lg:flex justify-center lg:text-left">
-      {/* <div className="grid grid-cols-2 p-8 w-3/5">
-        <div className="w-4/5">357 CONNECTIONS</div>
-        <div>Search</div>
-      </div> */}
-    </div>
+    <body>
+      <div className="lg:flex justify-center lg:text-left">
+        <div className="grid grid-cols-3 p-8 w-3/5 gap-1 text-lg">
+          <div className="col-span-2 font-bold">357 CONNECTIONS</div>
+          <div className="opacity-40">Search</div>
+        </div>
+      </div>
+    </body>
   );
 };
 
