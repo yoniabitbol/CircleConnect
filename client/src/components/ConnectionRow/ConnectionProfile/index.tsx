bc941d147a8a9da423304deab1ad2fe8da3d43c8
Ankita Ingle <aningle31@gmail.com>
2023-02-04 22:56:20 -0500

    Add placeholder image

--- client/src/components/ConnectionRow/ConnectionProfile/index.tsx
+++ client/src/components/ConnectionRow/ConnectionProfile/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import placeholder from "./placeholder_profile.png";
 
 const ConnectionProfile: React.FC = () => {
   return (
     <div className="lg:flex lg:text-left py-2">
       <div className="grid grid-cols-2">
         <div className="col-span-1">
-          <img src="..."></img>
+          <img src={placeholder} className="w-16 rounded-full"></img>
         </div>
         <div className="col-span-1">
           <p>First Name</p>
