81028c754acbfe661b4ee034116c80115d1548bc
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-02-06 15:53:10 -0500

    fixed the route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
               />
               <Route
                 path="notifications"
-                element={!user ? <Notifications /> : <Navigate to="/" />}
+                element={user ? <Notifications /> : <Navigate to="/" />}
               />
             </Route>
           </Routes>
