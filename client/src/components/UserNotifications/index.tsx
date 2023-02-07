71c7f39c4480260b89a76b5b1109713fe45b475a
lucadallaire <luca.dallaire@gmail.com>
2023-02-06 22:42:19 -0500

    added more to the notifications page

--- client/src/components/UserNotifications/index.tsx
+++ client/src/components/UserNotifications/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Alerts from "./Alerts";
 import Dashboard from "./Dashboard";
+import NavSettings from "./NavSettings";
 
 const UserNotifications: React.FC = () => {
   return (
-    <div className="grid grid-rows-3 grid-flow-col">
+    
+    <div className="grid grid-rows-4 grid-flow-col">
+      <div className="row-span-1">
+        <NavSettings />
+      </div>
       <div className="row-span-3">
         <Dashboard
           views_today="367"
@@ -12,7 +17,11 @@ const UserNotifications: React.FC = () => {
           search_appearances="9"
         />
       </div>
-      <div className="col-span-2 flex justify-center">RECENT</div>
+      <div className="row-span-1 col-span-2 flex justify-center">
+        <hr className="w-1/3 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+        <div className="">RECENT</div>
+        <hr className="w-1/3 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+      </div>
       <div className="row-span-2 col-span-2">
         <Alerts
           type="Alert"
