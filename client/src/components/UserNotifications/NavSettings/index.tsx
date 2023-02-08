f8c8d5f216211178d7479453eb6470350e6f4e09
lucadallaire <luca.dallaire@gmail.com>
2023-02-07 20:09:17 -0500

    made a few changes

--- client/src/components/UserNotifications/NavSettings/index.tsx
+++ client/src/components/UserNotifications/NavSettings/index.tsx
@@ -6,7 +6,7 @@ const NavSettings: React.FC = () => {
     <div className="lg:w-2/3 w-4/5 m-5 p-5 rounded-md bg-slate-100 mx-auto">
         <div className="flex flex-col justify-center mb-2">
             <Link to="/profile">NOTIFICATIONS</Link>
-            <hr className="w-5/6 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+            <hr className="w-full h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
             <Link to="/profile">NOTIFICATION SETTINGS</Link>
         </div>
   </div>
