f5b2b159239dcf52300f350526c64fa127754636
William Wells <williamwells2013@hotmail.com>
2023-02-08 02:21:20 -0500

    Add chat session item component

--- client/src/components/Chat/Sessions/index.tsx
+++ client/src/components/Chat/Sessions/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SessionItem from "../SessionItem";
 
 const Sessions: React.FC = () => {
   return (
@@ -7,10 +8,27 @@ const Sessions: React.FC = () => {
         <div className="justify-start ml-10 my-6">
           <span className="text-sm">CHATS</span>
         </div>
-        <hr className="my-2 border-gray-400 border" />
-        <div className="flex justify-start ml-5 my-5">
-          <span className="text-sm">Test</span>
-        </div>
+        <hr className="border-gray-400 border" />
+        <SessionItem
+          selected={false}
+          session={{
+            user: {
+              name: "Chad Thundercock",
+              picture: "https://cdn.discordapp.com/attachments/800804024109891595/1067509878534438972/IMG_20220119_162228_138.jpg",
+            },
+            latestMsg: "Hey gamer",
+          }}
+        />
+        <SessionItem
+          selected={true}
+          session={{
+            user: {
+              name: "Hungry Boi",
+              picture: "https://cdn.discordapp.com/attachments/672975677460447232/1072778010597535784/photo_2022-05-21_09-09-15.jpg",
+            },
+            latestMsg: "Are you food",
+          }}
+        />
       </div>
     </div>);
 };
