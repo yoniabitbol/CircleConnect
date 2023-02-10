17b6734b38cee6d46c84454e6a848d1ba8adc745
lucadallaire <luca.dallaire@gmail.com>
2023-02-10 16:45:43 -0500

    added the connections alert notifications

--- client/src/components/UserNotifications/NavSettings/index.tsx
+++ client/src/components/UserNotifications/NavSettings/index.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const NavSettings: React.FC = () => {
   return (
-    <div className="lg:w-2/3 w-4/5 m-5 p-5 rounded-md bg-slate-100 mx-auto">
+    <div className="lg:w-11/12 w-4/5 m-5 p-5 rounded-md bg-slate-100 mx-auto">
         <div className="flex flex-col justify-center mb-2">
             <Link to="/profile">NOTIFICATIONS</Link>
-            <hr className="w-full h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+            <hr className="w-full h-0.5 mx-auto my-2 bg-gray-200 border-0 rounded md:my-5 dark:bg-gray-300"/>
             <Link to="/profile">NOTIFICATION SETTINGS</Link>
         </div>
   </div>
