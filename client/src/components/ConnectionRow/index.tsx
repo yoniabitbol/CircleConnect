3495ce3ecb90698e3f8a48e17e7fc922c6b0bd51
Evan Greenstein <evanlg16@gmail.com>
2023-02-10 12:20:32 -0500

    Made a bit more responsive

--- client/src/components/ConnectionRow/index.tsx
+++ client/src/components/ConnectionRow/index.tsx
@@ -3,21 +3,20 @@ import placeholder from "./placeholder_profile.png";
 
 const ConnectionRow: React.FC = () => {
   return (
-    <div className="flex justify-center lg:text-left md:text-left text-center py-2">
-      <div className="grid lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-1 p-4 lg:text-sm md:text-xs sm:text-xs lg:w-7/12 bg-white">
-        <div className="lg:col-span-1 md:col-span-1">
+    <div className="flex justify-center sm:text-left text-center py-2">
+      <div className="flex items-center gap-4 p-4 sm:text-sm text-xs w-full sm:w-7/12 bg-white">
+        <div className="">
           <img
             src={placeholder}
             className="w-16 rounded-full md:align-center"
           ></img>
         </div>
-        <div className="lg:col-span-1 md:col-span-1 py-2">
+        <div className="grow py-2">
           <p>First Name</p>
           <p>Position</p>
           <p>100 Connections</p>
         </div>
-        <div className="lg:col-span-2 md:col-span-1"></div>
-        <div className="lg:col-span-1 md:col-span-1">
+        <div className="">
           <button
             type="submit"
             className="block lg:mt-4 w-auto px-5 py-2 rounded-md bg-signup-button
@@ -26,7 +25,7 @@ const ConnectionRow: React.FC = () => {
             MESSAGE
           </button>
         </div>
-        <div className="lg:col-span-1 md:col-span-1">
+        <div className="">
           <button type="submit">•••</button>
         </div>
       </div>
