a467c978c314ff1b21dc3d784a41f4c16423e1b8
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-02-10 01:44:49 -0500

    added the language selector in the footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable max-len */
 import React from "react";
+import LanguageDropdown from "./LanguageDropdown";
 
 const Footer: React.FC = () => {
   return (
@@ -14,13 +15,14 @@ const Footer: React.FC = () => {
           />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">CircleConnect</span>
             </a>
-        <div className="flex items-center md:order-2">
+        {/* <div className="flex items-center md:order-2">
             <button type="button" data-dropdown-toggle="language-dropdown-menu" className="inline-flex items-center justify-center px-4 py-2 text-sm text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 light:hover:bg-gray-700 light:hover:text-white">
                 <svg className="w-5 h-5 mr-2 rounded-full" aria-hidden="true" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" stroke-width="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/></svg>
                 English (US)
             </button>
             
-        </div>
+        </div> */}
+        <LanguageDropdown/>
         </div>
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">CircleConnect</a>. All Rights Reserved.
         </span>
