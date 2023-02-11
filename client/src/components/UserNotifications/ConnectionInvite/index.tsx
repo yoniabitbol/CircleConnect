eedd63e355110f3e9645ab7eefc52528e363c3a1
lucadallaire <luca.dallaire@gmail.com>
2023-02-10 21:24:34 -0500

    made small changes and added more notifications to page

--- client/src/components/UserNotifications/ConnectionInvite/index.tsx
+++ client/src/components/UserNotifications/ConnectionInvite/index.tsx
@@ -11,9 +11,9 @@ const ConnectionInvite: React.FC<{
   const { name, job_title, connections, connection_message } = props;
   const [userProfilePic] = React.useState<string>();
   return (
-    <div className="lg:w-11/12 w-4/5 m-5 p-5 rounded-md bg-slate-100 mx-auto">
-      <div className="flex flex-row justify-between">
-        <div><Link to="/profile"><Avatar src={userProfilePic}/></Link></div>
+    <div className="lg:w-9/12 w-4/5 m-5 p-5 rounded-md bg-slate-100 mx-auto">
+      <div className="flex flex-row">
+        <div className="w-1/12"><Link to="/profile"><Avatar src={userProfilePic}/></Link></div>
         <div className="flex flex-row justify-between">
             <div className="flex flex-col">
                 <h1 className="text-xl font-semibold pt-2">{name}</h1>
@@ -25,8 +25,19 @@ const ConnectionInvite: React.FC<{
                 <h3 className="text-sm text-gray-400">{connection_message}</h3>
             </div>
             <div className="flex flex-row">
-                <button>ACCEPT</button>
-                <button>DECLINE</button>
+            <button
+              type="submit"
+              className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-2 rounded-md m-5"
+              
+            >
+              ACCEPT
+            </button>
+            <button
+              type="submit"
+              className="bg-slate-400 hover:bg-slate-500 text-white py-2 px-2 rounded-md m-5"
+            >
+              DECLINE
+            </button>
             </div>
         </div>
       </div>
--- client/src/components/UserNotifications/index.tsx
+++ client/src/components/UserNotifications/index.tsx
@@ -6,34 +6,44 @@ import ConnectionInvite from "./ConnectionInvite";
 
 const UserNotifications: React.FC = () => {
   return (
-    <div className="grid grid-rows-4 grid-flow-col flex justify-center mt-6">
-      <div className="row-span-1">
+    <div className="flex flex-row justify-center mt-6">
+      <div className="flex flex-col">
         <NavSettings />
-      </div>
-      <div className="row-span-3">
         <Dashboard
           views_today="367"
           posts_views="15"
           search_appearances="9"
         />
       </div>
-      <div className="h-1/6 row-span-1 col-span-2 flex justify-center">
-        <hr className="w-1/3 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
-        <div className="">RECENT</div>
-        <hr className="w-1/3 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
-      </div>
-      <div className="row-span-2 col-span-2">
-        <Alert
-          type="Alert"
-          description="Samuel Jackson accepted your connection request"
-          time="3 hours"
-        />
-        <ConnectionInvite
-          name="Brandon Wilson"
-          job_title="Senior UX Designer"
-          connections={623}
-          connection_message="Hey, I saw your works. I like it! Can we do something together? Or maybe you have project for UX at the moment?"
-          />
+      <div className="flex flex-col">
+          <div className="flex flex-row justify-center">
+            <hr className="w-1/4 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+            <div className="">RECENT</div>
+            <hr className="w-1/4 h-0.5 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-300"/>
+          </div>
+          <div className="row-span-2 col-span-2">
+            <Alert
+              type="Alert"
+              description="Samuel Jackson accepted your connection request"
+              time={3}
+            />
+            <ConnectionInvite
+              name="Brandon Wilson"
+              job_title="Senior UX Designer"
+              connections={623}
+              connection_message="Hey, I saw your works. I like it! Can we do something together? Or maybe you have project for UX at the moment?"
+              />
+            <Alert
+              type="Alert"
+              description="Audrey Alexander and 10 others viewed your profile"
+              time={9}
+            />
+            <Alert
+              type="Alert"
+              description="We found jobs that you may be interested"
+              time={15}
+            />
+          </div>
       </div>
     </div>
   );
