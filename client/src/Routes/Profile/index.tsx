244a0ea15caa3485c52c221525a20aaaa508ca56
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-12 22:43:32 -0500

    started dynamic routing

--- client/src/Routes/Profile/index.tsx
+++ client/src/Routes/Profile/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import UserProfile from "../../components/UserProfile";
 import getCurrentUserProfile from "../../http/getCurrentUserProfile";
+import getUserProfile from "../../http/getUserProfile";
 import Usertypes from "../../Models/UserProfileModel";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const Profile: React.FC = () => {
+interface Props {
+  id?: string;
+}
+
+const Profile: React.FC<Props> = ({ id }) => {
+  const params = useParams<{ id?: string }>();
+  const userId = id || params.id;
   const [myUser, setMyUser] = useState<Usertypes>({
     name: "",
     title: "",
@@ -26,11 +34,17 @@ const Profile: React.FC = () => {
   });
 
   useEffect(() => {
-    getCurrentUserProfile().then((res) => {
-      setMyUser(res.data.user);
-    });
+    if (userId) {
+      getUserProfile(userId).then((res) => {
+        setMyUser(res.data.user);
+      });
+    } else {
+      getCurrentUserProfile().then((res) => {
+        setMyUser(res.data.user);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   return (
     <>
