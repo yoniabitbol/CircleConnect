05663d497ae56ec99e0da0d6eac5a6e2ba7753f2
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-14 18:01:57 -0500

    fixed layout margin

--- client/src/components/MyUserProfile/Layout/layout.tsx
+++ client/src/components/MyUserProfile/Layout/layout.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from "react";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="flex flex-row gap-5 px-32 w-full">{children}</div>;
+  return (
+    <div className="flex flex-row gap-5 px-32 w-full my-5">{children}</div>
+  );
 };
 
 export default Layout;
