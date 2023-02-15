e21832257f634ed9d5a3c0da8d5b5f07f0e0e935
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-14 19:04:16 -0500

    simplified interface

--- client/src/http/updateUserProfile.ts
+++ client/src/http/updateUserProfile.ts
@@ -1,70 +1,10 @@
 import { auth } from "../firebase/config";
+import Usertypes from "../Models/UserProfileModel";
 
 const port = process.env.REACT_APP_BACKEND_PORT || 4000;
 const url = `http://localhost:${port}/api/users`;
 
-interface Usertypes {
-  name: string;
-  title: string;
-  location: string;
-  email: string;
-  phone: string;
-  website: string;
-  connections: number;
-  picture: string;
-  backdrop: string;
 
-  summary: string;
-  projects: {
-    title: string;
-    description: string;
-    startDate: string;
-    endDate: string;
-    technologies: string[];
-    picture: string;
-  }[];
-  skills: {
-    name: string;
-    level: string;
-  }[];
-  experience: {
-    company: string;
-    logo: string;
-    title: string;
-    location: string;
-    startDate: string;
-    endDate: string;
-    description: string;
-  }[];
-  education: {
-    school: string;
-    logo: string;
-    degree: string;
-    location: string;
-    startDate: string;
-    endDate: string;
-    description: string;
-  }[];
-  languages: {
-    name: string;
-    level: string;
-  }[];
-  awards: {
-    title: string;
-    date: string;
-    awarder: string;
-    summary: string;
-  }[];
-  courses: {
-    title: string;
-    number: string;
-    school: string;
-    startDate: string;
-    endDate: string;
-    description: string;
-  }[];
-}
-//review UserTypes and import it from somewhere else, do not keep in here
 async function updateUserProfile(profile: Usertypes) {
   const user = auth.currentUser;
   const token = user && (await user.getIdToken());
