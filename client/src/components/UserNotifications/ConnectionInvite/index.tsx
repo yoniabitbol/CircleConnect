a36c5643bd405d127d6a550753d95ac37cd416c6
Ankita Ingle <aningle31@gmail.com>
2023-02-16 08:55:25 -0500

    Fix Accept and Decline button styles

--- client/src/components/UserNotifications/ConnectionInvite/index.tsx
+++ client/src/components/UserNotifications/ConnectionInvite/index.tsx
@@ -14,31 +14,38 @@ const ConnectionInvite: React.FC<{
     <div className="lg:w-9/12 w-full m-5 p-5 rounded-md bg-slate-100 mx-auto">
       <div className="flex flex-row justify-between">
         <div className="flex flex-row space-x-8">
-          <div className="lg:w-1/12"><Link to="/profile"><Avatar src={userProfilePic}/></Link></div>
+          <div className="lg:w-1/12">
+            <Link to="/profile">
+              <Avatar src={userProfilePic} />
+            </Link>
+          </div>
           <div className="flex flex-col">
-                  <h1 className="text-sm lg:text-xl font-semibold pt-2">{name}</h1>
-                  <h2 className="text-xs lg:text-sm">{job_title}</h2>
-                  <h3 className="text-xs lg:text-sm" style={{color: '#4B47B7'}}>{connections} connections</h3>
+            <h1 className="text-sm lg:text-xl font-semibold pt-2">{name}</h1>
+            <h2 className="text-xs lg:text-sm">{job_title}</h2>
+            <h3 className="text-xs lg:text-sm" style={{ color: "#4B47B7" }}>
+              {connections} connections
+            </h3>
           </div>
         </div>
         <div className="hidden md:flex flex-row w-1/3">
-                <h1 className="text-6xl" style={{color: '#4B47B7'}}>|</h1>
-                <h3 className="text-sm text-gray-400">{connection_message}</h3>
+          <h1 className="text-6xl" style={{ color: "#4B47B7" }}>
+            |
+          </h1>
+          <h3 className="text-sm text-gray-400">{connection_message}</h3>
         </div>
         <div className="flex flex-row space-x-1 lg:space-x-4 mt-4">
-                <button
-                type="submit"
-                className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-2 rounded-md lg:text-lg text-xs lg:w-21 w-15 lg:h-10 h-8"
-                
-                >
-                ACCEPT
-                </button>
-                <button
-                type="submit"
-                className="bg-slate-400 hover:bg-slate-500 text-white py-2 px-2 rounded-md lg:text-lg text-xs lg:w-21 w-15 lg:h-10 h-8"
-                >
-                DECLINE
-                </button>
+          <button
+            type="submit"
+            className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md text-sm w-24 h-8"
+          >
+            ACCEPT
+          </button>
+          <button
+            type="submit"
+            className="bg-slate-400 hover:bg-slate-500 text-white rounded-md text-sm w-24 h-8"
+          >
+            DECLINE
+          </button>
         </div>
       </div>
     </div>
