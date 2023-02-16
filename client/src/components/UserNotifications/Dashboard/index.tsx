0a894c25e821dce67f7fa987c87117ce6dc641b1
Ankita Ingle <aningle31@gmail.com>
2023-02-16 11:19:11 -0500

    Fix left padding for dashboard

--- client/src/components/UserNotifications/Dashboard/index.tsx
+++ client/src/components/UserNotifications/Dashboard/index.tsx
@@ -21,18 +21,20 @@ const Dashboard: React.FC<{
           </Link>
         </div>
         <hr className="w-full h-px mx-auto my-2 bg-gray-200 border-0 rounded md:my-5 dark:bg-gray-300" />
-        <h1 className="text-4xl font-semibold" style={{ color: "#4B47B7" }}>
-          {views_today}
-        </h1>
-        <h1 className="">views today</h1>
-        <h2 className="text-4xl font-semibold" style={{ color: "#4B47B7" }}>
-          {posts_views}
-        </h2>
-        <h2 className="">posts views</h2>
-        <h3 className="text-4xl font-semibold" style={{ color: "#4B47B7" }}>
-          {search_appearances}
-        </h3>
-        <h3 className="">search appearances</h3>
+        <div className="px-6">
+          <h1 className="text-5xl font-semibold" style={{ color: "#4B47B7" }}>
+            {views_today}
+          </h1>
+          <h1 className="">views today</h1>
+          <h2 className="text-5xl font-semibold" style={{ color: "#4B47B7" }}>
+            {posts_views}
+          </h2>
+          <h2 className="">posts views</h2>
+          <h3 className="text-5xl font-semibold" style={{ color: "#4B47B7" }}>
+            {search_appearances}
+          </h3>
+          <h3 className="">search appearances</h3>
+        </div>
       </div>
     </div>
   );
