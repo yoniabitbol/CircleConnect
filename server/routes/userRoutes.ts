e71bff2fd4b51e8e0fa41b04708f98a7835c9ca6
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-16 19:36:07 -0500

    fixed typo

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,26 +1,34 @@
-import express from 'express';
-import UserController from '../controllers/userController';
-import { uploadImages, resizePhoto } from '../middleware/multer';
+import express from "express";
+import UserController from "../controllers/userController";
+import { uploadImages, resizePhoto } from "../middleware/multer";
 
 const router = express.Router();
 
 // User Profile Routes
-router.route('/')
+router
+  .route("/")
   .get(UserController.getAllUsers)
   .post(UserController.createUser)
-  .patch(UserController.updateUser);
+  .patch(
+    uploadImages,
+    resizePhoto,
+    UserController.updateUser
+  );
 
-router.route('/:user_id')
+router
+  .route("/:user_id")
   .get(UserController.getUser)
   .delete(UserController.deleteUser);
 
 // User Connections Routes
-router.route('/:user_id/connections').get(UserController.getUserConnections);
-router.route('/:user_id/incoming').get(UserController.getIncomingRequests);
-router.route('/:user_id/outgoing').get(UserController.getOutgoingRequests);
-router.route('/:user_id/connect').patch(UserController.sendConnectionRequest);
-router.route('/:user_id/accept').patch(UserController.acceptConnectionRequest);
-router.route('/:user_id/decline').patch(UserController.declineConnectionRequest);
-router.route('/:user_id/remove').patch(UserController.removeConnection);
+router.route("/:user_id/connections").get(UserController.getUserConnections);
+router.route("/:user_id/incoming").get(UserController.getIncomingRequests);
+router.route("/:user_id/outgoing").get(UserController.getOutgoingRequests);
+router.route("/:user_id/connect").patch(UserController.sendConnectionRequest);
+router.route("/:user_id/accept").patch(UserController.acceptConnectionRequest);
+router
+  .route("/:user_id/decline")
+  .patch(UserController.declineConnectionRequest);
+router.route("/:user_id/remove").patch(UserController.removeConnection);
 
 export default router;
