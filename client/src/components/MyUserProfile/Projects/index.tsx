8af2996e177a7b9172241dd8c8e58677fddf688d
Evan Greenstein <evanlg16@gmail.com>
2023-02-18 13:50:38 -0500

    Created prototype project form and cards

--- client/src/components/MyUserProfile/Projects/index.tsx
+++ client/src/components/MyUserProfile/Projects/index.tsx
@@ -1,5 +1,6 @@
 import { Field } from "formik";
 import React, { useState } from "react";
+import Divider from "@mui/material/Divider";
 
 const Projects: React.FC<{
   projects: {
@@ -13,102 +14,128 @@ const Projects: React.FC<{
   edit: boolean;
   formik: any;
 }> = ({ projects, edit }) => {
+  console.log(projects);
+
   const [addNew, setAddNew] = useState(false);
 
   const addNewBtn = (
     <button
       onClick={() => setAddNew(true)}
-      className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full m-5"
+      className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full my-5"
     >
       Add new
     </button>
   );
 
+  function projectFields(index: number) {
+    return (
+      <>
+        <label className="text-sm font-semibold text-gray-600 py-2">
+          Title
+        </label>
+        <Field
+          name={`projects[${index}].title`}
+          className="w-full rounded-sm"
+          type="text"
+          as="input"
+        />
+        <label className="text-sm font-semibold text-gray-600 py-2">
+          Description
+        </label>
+        <Field
+          name={`projects[${index}].description`}
+          className="w-full rounded-sm"
+          type="text"
+          as="input"
+        />
+        <label className="text-sm font-semibold text-gray-600 py-2">
+          Start Date
+        </label>
+        <Field
+          name={`projects[${index}].startDate`}
+          className="w-full rounded-sm"
+          type="text"
+          as="input"
+        />
+        <label className="text-sm font-semibold text-gray-600 py-2">
+          End Date
+        </label>
+        <Field
+          name={`projects[${index}].endDate`}
+          className="w-full rounded-sm"
+          type="text"
+          as="input"
+        />
+        <label className="text-sm font-semibold text-gray-600 py-2">
+          Technologies (seperate with a comma)
+        </label>
+        <Field
+          as="input"
+          name={`projects[${index}].technologies`}
+          className="w-full rounded-sm"
+          type="text"
+        />
+      </>
+    );
+  }
+
+  const existingProjects = projects.map((proj, index) => {
+    return (
+      <>
+        <div key={proj.title} className="my-5">
+          {projectFields(index)}
+        </div>
+        <Divider />
+      </>
+    );
+  });
+
   const form = addNew ? (
-    <div className="w-2/3 mx-auto pt-5">
-      <label className="text-sm font-semibold text-gray-600 py-2">Title</label>
-      <Field name="projects.title" className="w-full rounded-sm" type="text" />
-      <label className="text-sm font-semibold text-gray-600 py-2">
-        Description
-      </label>
-      <Field
-        name="projects.description"
-        className="w-full rounded-sm"
-        type="text"
-      />
-      <label className="text-sm font-semibold text-gray-600 py-2">
-        Start Date
-      </label>
-      <Field
-        name="projects.startDate"
-        className="w-full rounded-sm"
-        type="text"
-      />
-      <label className="text-sm font-semibold text-gray-600 py-2">
-        End Date
-      </label>
-      <Field
-        name="projects.endDate"
-        className="w-full rounded-sm"
-        type="text"
-      />
-      <label className="text-sm font-semibold text-gray-600 py-2">
-        Technologies (seperate with a comma)
-      </label>
-      <Field
-        name="projects.technologies"
-        className="w-full rounded-sm"
-        type="text"
-      />
-      {/* <label className="text-sm font-semibold text-gray-600 py-2">
-        Project picture
-      </label>
-      <input
-        id="file"
-        name="projectimg"
-        type="file"
-        className="w-full rounded-sm"
-        onChange={(event) => {
-          const file: FileList | null = event.currentTarget.files;
-          if (!file) return;
-          else {
-            formik.setFieldValue("projectimg", file[0]);
-          }
-        }}
-      /> */}
+    <div className="pt-5">
+      {existingProjects}
+      {projectFields(projects.length)}
     </div>
   ) : (
-    addNewBtn
+    <div>
+      {existingProjects}
+      {addNewBtn}
+    </div>
   );
-  
+
   const component = edit ? (
     form
   ) : projects.length < 2 ? (
-    <p>No projects yet</p>
+    <p>Create at least 2 projects</p>
   ) : (
-    projects?.map((project, index) => {
-      return (
-        <div key={index} className="flex flex-col justify-center mb-5">
-          <h1 className="text-lg font-semibold pt-2">{project.title}</h1>
-          <h2 className="">{project.description}</h2>
-          <h3 className="pt-2">
-            {project.startDate} -&gt; {project.endDate}{" "}
-          </h3>
+    <div className="grid grid-cols-3 gap-4 my-4">
+      {projects?.map((project) => {
+        return (
+          <div
+            key={project.title}
+            className="flex flex-col justify-center bg-white rounded-md p-5"
+          >
+            <h1 className="text-lg font-semibold pt-2">{project.title}</h1>
+            <h2 className="">{project.description}</h2>
+            <h4 className="">
+              {project.startDate} → {project.endDate}
+            </h4>
 
-          <div className="flex space-x-5 pt-2">
-            {project.technologies?.split(",").map((technology, index) => {
-              return <div key={index}>{technology}</div>;
-            })}
+            <div className="flex flex-wrap gap-2 pt-2">
+              {project.technologies?.split(",").map((technology) => {
+                return (
+                  <div
+                    key={technology}
+                    className="rounded-md bg-slate-500 hover:bg-slate-700 px-2"
+                  >
+                    {technology}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          {/* 
-          <img
-            className="w-32 h-32 rounded-full border-2 border-white"
-            src={project.picture}
-            alt="project"
-          /> */}
-        </div>
-      );
-    })
+        );
+      })}
+    </div>
   );
 
   return (
