37e306e4d1be6dbcbb1155609409d8a65390ea6b
Reuven1203 <reuven1203@gmail.com>
2023-02-18 21:50:10 -0500

    Search connections functionality

--- client/src/Routes/Network/index.tsx
+++ client/src/Routes/Network/index.tsx
@@ -9,24 +9,38 @@ type ConnectionType = Omit<Usertypes, "location" | "email" | "phone" | "website"
 
   const Network: React.FC = () => {
 const [connections, setConnections] = useState<any>([]);
+const [search, setSearch] = useState<string>("");
+const [filteredConnections, setFilteredConnections] = useState<any>([]);
 
   useEffect(() => {
     getUserConnections().then((res) => {
       setConnections(res.data.connections);
+      setFilteredConnections(connections)
     });
   }, []);
-
-
-
+  
+  const onInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    if(e.target.value === ""){
+        setFilteredConnections(connections);
+    }else{
+      const filteredConnections = connections.filter((connection: ConnectionType) => {
+        return connection.name.toLowerCase().includes(e.target.value.toLowerCase());
+      });
+      setFilteredConnections(filteredConnections);
+    }
+    
+    }
+    
   return (
     <body style={{ backgroundColor: "#F7F9FB" }}>
       <div className="flex justify-center sm:text-left py-2">
         <div className="grid grid-cols-4 flex items-center gap-4 p-4 sm:text-sm text-xs w-full sm:w-7/12 bg-white">
           <div className="col-span-3 font-bold">{connections.length} CONNECTIONS</div>
-          <input placeholder="Search" className="border p-2"></input>
+          <input placeholder="Search" onChange={onInputChangeHandler} value={search} className="border p-2"></input>
         </div>
       </div>
-      {connections.map((connection: ConnectionType) => {
+      {filteredConnections.map((connection: ConnectionType) => {
         return (
           <ConnectionRow
             name={connection.name}
