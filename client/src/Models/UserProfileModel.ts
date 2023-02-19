345b38bbd64c8c8a8acbac838712e5b334034897
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-18 19:26:29 -0500

    Update UserProfileModel.ts

--- client/src/Models/UserProfileModel.ts
+++ client/src/Models/UserProfileModel.ts
@@ -1,61 +1,62 @@
 export default interface Usertypes {
+  user_id?: string;
+  name: string;
+  title: string;
+  location: string;
+  email: string;
+  phone: string;
+  website: string;
+  connections: number;
+  picture: string;
+  backdrop: string;
+
+  summary: string;
+  projects: {
+    title: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    technologies: string;
+    picture: string;
+  }[];
+  skills: {
     name: string;
+    level: string;
+  }[];
+  experience: {
+    company: string;
+    logo: string;
     title: string;
     location: string;
-    email: string;
-    phone: string;
-    website: string;
-    connections: number;
-    picture: string;
-    backdrop: string;
-  
+    startDate: string;
+    endDate: string;
+    description: string;
+  }[];
+  education: {
+    school: string;
+    logo: string;
+    degree: string;
+    location: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+  }[];
+  languages: {
+    name: string;
+    level: string;
+  }[];
+  awards: {
+    title: string;
+    date: string;
+    awarder: string;
     summary: string;
-    projects: {
-      title: string;
-      description: string;
-      startDate: string;
-      endDate: string;
-      technologies: string;
-      picture: string;
-    }[];
-    skills: {
-      name: string;
-      level: string;
-    }[];
-    experience: {
-      company: string;
-      logo: string;
-      title: string;
-      location: string;
-      startDate: string;
-      endDate: string;
-      description: string;
-    }[];
-    education: {
-      school: string;
-      logo: string;
-      degree: string;
-      location: string;
-      startDate: string;
-      endDate: string;
-      description: string;
-    }[];
-    languages: {
-      name: string;
-      level: string;
-    }[];
-    awards: {
-      title: string;
-      date: string;
-      awarder: string;
-      summary: string;
-    }[];
-    courses: {
-      title: string;
-      number: string;
-      school: string;
-      startDate: string;
-      endDate: string;
-      description: string;
-    }[];
-} 
+  }[];
+  courses: {
+    title: string;
+    number: string;
+    school: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+  }[];
+}
