f75984daf20de587f20663610b9fec9d3c334206
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-20 15:15:44 -0500

    mobile fix

--- client/src/components/MyUserProfile/Layout/rightSection.tsx
+++ client/src/components/MyUserProfile/Layout/rightSection.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable object-curly-newline */
 import React from "react";
 
-const RightSection: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="flex flex-col gap-5 w-1/3">{children}</div>;
+const RightSection: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div className="flex flex-col gap-5 lg:w-1/3 w-full lg:mt-0 mt-5">
+      {children}
+    </div>
+  );
 };
 
 export default RightSection;
