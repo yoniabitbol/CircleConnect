27b6d80619144cb868e4b20c802354822727982f
mattmazzone <73369339+mattmazzone@users.noreply.github.com>
2023-02-22 15:32:33 -0500

    Update index.tsx

--- client/src/components/Footer/LanguageDropdown/index.tsx
+++ client/src/components/Footer/LanguageDropdown/index.tsx
@@ -13,12 +13,16 @@ const LanguageDropdown: React.FC = () => {
           className="inline-flex items-center justify-center px-4 py-2 text-sm text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 light:hover:bg-gray-700 light:hover:text-white"
         >
           <svg
-            className="w-5 h-5 mr-2 rounded-full stroke-white stroke-300"
+            className="w-5 h-5 mr-2 rounded-full"
             aria-hidden="true"
             viewBox="0 0 3900 3900"
           >
             <path fill="#b22234" d="M0 0h7410v3900H0z" />
-            <path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" />
+            <path
+              d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
+              stroke="#fff"
+              stroke-width="300"
+            />
             <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
           </svg>
           English
