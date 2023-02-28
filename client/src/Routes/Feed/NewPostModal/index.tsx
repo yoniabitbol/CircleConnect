5871d790a3bc293310e857b541fae2589ef8cf85
Reuven1203 <reuven1203@gmail.com>
2023-02-28 04:07:23 -0500

    Grow Transition

--- client/src/Routes/Feed/NewPostModal/index.tsx
+++ client/src/Routes/Feed/NewPostModal/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import {Card, Modal, CardActions, TextareaAutosize, Button, IconButton, FormControlLabel, Checkbox, Tooltip} from '@mui/material';
+import {Grow,Card, Modal, CardActions, TextareaAutosize, Button, IconButton, FormControlLabel, Checkbox, Tooltip} from '@mui/material';
 import styles from './style.module.css';
 import {Send, Videocam, InsertPhoto, AttachFile, Help} from '@mui/icons-material';
 
 const style = {
     position: 'absolute',
-    top: '50%',
-    left: '50%',
+    top: '25%',
+    left: '25%',
     transform: 'translate(-50%, -50%)',
     width: 700,
     height:300,
@@ -16,18 +16,19 @@ const style = {
 const NewPostModal:React.FC<{showModal: boolean, handleModalClose:()=>void}> = (props) => {
     const {showModal, handleModalClose} = props;
     return (
-        <div>
-            <Modal
-                open={showModal}
-                onClose={handleModalClose}>
-                <Card className={styles.modal} sx={style}>
-                    <div className="w-full sticky top-0 bg-white p-2 z-20">
-                        <h6 className="font-bold p-3">NEW POST</h6>
-                        <hr className="ml-4 w-9/10 bg-gray-300"/>
-                    </div>
-                    <div className="p-2">
-                        <TextareaAutosize minRows={7} maxRows={7} className="w-full  outline-none relative resize-none" placeholder="Whats on your mind?"/>
-                    </div>
+
+                <Modal
+                    open={showModal}
+                    onClose={handleModalClose}>
+                    <Grow in={showModal} >
+                    <Card className={styles.modal} sx={style}>
+                        <div className="w-full sticky top-0 bg-white p-2 z-20">
+                            <h6 className="font-bold p-3">NEW POST</h6>
+                            <hr className="ml-4 w-9/10 bg-gray-300"/>
+                        </div>
+                        <div className="p-2">
+                            <TextareaAutosize minRows={7} maxRows={7} className="w-full  outline-none relative resize-none" placeholder="Whats on your mind?"/>
+                        </div>
                         <CardActions className="fixed bottom-0 w-full p-2 z-20">
                             <FormControlLabel  control={<Checkbox defaultChecked sx={{color:'#4D47C3','&.Mui-checked': {color: '#4D47C3',}}}/>} color='success' label="This is a job posting"/>
                             <Tooltip title="Checking this will allow users to submit Resumés/CV to apply" sx={{color:'#4D47C3', backgroundColor:'white'}} placement="top">
@@ -53,9 +54,11 @@ const NewPostModal:React.FC<{showModal: boolean, handleModalClose:()=>void}> = (
                                 </Button>
                             </div>
                         </CardActions>
-                </Card>
-            </Modal>
-        </div>
+                    </Card>
+                    </Grow>
+                </Modal>
+
+
     );
 }
 
