c46ac1cb6876e05e5b10e59464245d6cbf2ea4c0
jonabitbol <tbnrsamurai@gmail.com>
2023-02-27 21:37:59 -0500

    added post model

---
+++ server/models/postModel.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  postID: {
+    type: String,
+  },
+  creatorID: {
+    type: String,
+  },
+  isJobListing: {
+    type: Boolean,
+  },
+  createdAt: {
+    type: Date,
+  },
+  text: {
+    type: String,
+  },
+  picture: {
+    type: String,
+  },
+  likes: {
+    type: Number,
+  },
+  comments: [{
+    type: String,
+  }],
+  tags: [{
+    type: String,
+  }],
+  uploadDeadline: {
+    type: Date,
+  },
+  isThirdParty: {
+    type: Boolean,
+  },
+  requiredDocuments: [{
+    type: String,
+  }],
+  applications: [{
+    type: String,
+  }],
+});
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;
