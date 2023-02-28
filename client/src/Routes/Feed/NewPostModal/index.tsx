f3005ef2ac6315168db3329efb20b5ef8accecfb
Reuven1203 <reuven1203@gmail.com>
2023-02-28 04:43:44 -0500

    fixed responsiveness

--- client/src/Routes/Feed/NewPostModal/index.tsx
+++ client/src/Routes/Feed/NewPostModal/index.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
-import {Grow,Card, Modal, CardActions, TextareaAutosize, Button, IconButton, FormControlLabel, Checkbox, Tooltip} from '@mui/material';
+import {Card, Modal, CardActions, TextareaAutosize, Button, IconButton, FormControlLabel, Checkbox, Tooltip} from '@mui/material';
 import styles from './style.module.css';
 import {Send, Videocam, InsertPhoto, AttachFile, Help} from '@mui/icons-material';
 
 const style = {
     position: 'absolute',
-    top: '25%',
-    left: '25%',
+    top: '50%',
+    left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 700,
-    height:300,
     bgColor: 'white',
 };
 
@@ -19,8 +17,8 @@ const NewPostModal:React.FC<{showModal: boolean, handleModalClose:()=>void}> = (
 
                 <Modal
                     open={showModal}
-                    onClose={handleModalClose}>
-                    <Grow in={showModal} >
+                    onClose={handleModalClose}
+                >
                     <Card className={styles.modal} sx={style}>
                         <div className="w-full sticky top-0 bg-white p-2 z-20">
                             <h6 className="font-bold p-3">NEW POST</h6>
@@ -29,12 +27,14 @@ const NewPostModal:React.FC<{showModal: boolean, handleModalClose:()=>void}> = (
                         <div className="p-2">
                             <TextareaAutosize minRows={7} maxRows={7} className="w-full  outline-none relative resize-none" placeholder="Whats on your mind?"/>
                         </div>
-                        <CardActions className="fixed bottom-0 w-full p-2 z-20">
-                            <FormControlLabel  control={<Checkbox defaultChecked sx={{color:'#4D47C3','&.Mui-checked': {color: '#4D47C3',}}}/>} color='success' label="This is a job posting"/>
-                            <Tooltip title="Checking this will allow users to submit Resumés/CV to apply" sx={{color:'#4D47C3', backgroundColor:'white'}} placement="top">
-                                <Help/>
-                            </Tooltip>
-                            <div className="absolute right-0 flex space-x-1.5 px-2">
+                        <CardActions className="fixed bottom-0 flex w-full p-2 z-20">
+                            <div className="flex w-1/2 items-center space-x-0">
+                                <Tooltip className="max-sm:hidden" title="Checking this will allow users to submit Resumés/CV to apply" sx={{color:'#4D47C3', backgroundColor:'white'}} placement="top">
+                                    <Help/>
+                                </Tooltip>
+                                <FormControlLabel className="w-fit"  control={<Checkbox defaultChecked sx={{color:'#4D47C3','&.Mui-checked': {color: '#4D47C3',}}}/>} color='success' label="This is a job posting"/>
+                            </div>
+                            <div className="absolute right-0 flex w-1/2 space-x-1 px-2 justify-end">
                                 <IconButton>
                                     <AttachFile/>
                                 </IconButton>
@@ -55,7 +55,6 @@ const NewPostModal:React.FC<{showModal: boolean, handleModalClose:()=>void}> = (
                             </div>
                         </CardActions>
                     </Card>
-                    </Grow>
                 </Modal>
 
 
