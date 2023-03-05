fb832e4e6151f41c15d317f1210471acdc3b3931
jonabitbol <tbnrsamurai@gmail.com>
2023-03-05 13:12:58 -0500

    added timestamps to applications

--- server/models/applicationModel.ts
+++ server/models/applicationModel.ts
@@ -1,34 +1,37 @@
 import mongoose from 'mongoose';
 
-const applicationSchema = new mongoose.Schema({
-  applicationID: {
-    type: String,
+const applicationSchema = new mongoose.Schema(
+  {
+    applicationID: {
+      type: String,
+    },
+    applicantID: {
+      type: String,
+      ref: 'User',
+    },
+    postID: {
+      type: String,
+      ref: 'Post',
+    },
+    text: {
+      type: String,
+    },
+    resume: {
+      type: String,
+    },
+    coverLetter: {
+      type: String,
+    },
+    existingInfo: {
+      type: Boolean,
+      default: false,
+    },
   },
-  createdAt: {
-    type: Date,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  applicantID: {
-    type: String,
-    ref: 'User',
-  },
-  postID: {
-    type: String,
-    ref: 'Post',
-  },
-  text: {
-    type: String,
-  },
-  resume: {
-    type: String,
-  },
-  coverLetter: {
-    type: String,
-  },
-  existingInfo: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 const Application = mongoose.model('Application', applicationSchema);
 export default Application;
