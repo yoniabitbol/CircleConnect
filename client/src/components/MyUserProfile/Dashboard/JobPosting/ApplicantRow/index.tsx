4d664249e37c0ac585fc038e4b852f896b8e8dd3
Ankita Ingle <aningle31@gmail.com>
2023-03-06 14:08:38 -0500

    Remove profile photo

--- client/src/components/MyUserProfile/Dashboard/JobPosting/ApplicantRow/index.tsx
+++ client/src/components/MyUserProfile/Dashboard/JobPosting/ApplicantRow/index.tsx
@@ -1,26 +1,24 @@
-import placeholder from "../placeholder.png";
+// import placeholder from "../placeholder.png";
 
 const JobPosting: React.FC = () => {
   return (
     <div>
-      <div className="flex mt-2 text-xs">
-        <div className="ml-2 mr-4 my-3">
-          <img
-            src={placeholder}
-            className="w-10 rounded-full md:align-center"
-          ></img>
+      <div className="flex mt-2 lg:text-xs mr-3 bg-input-purple px-2 py-2">
+        <a href="/" className="mx-2">
+          Applicant Name
+        </a>
+        <div className="">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className="text-xs block w-auto px-3 py-1 rounded-md bg-signup-button
+            text-white hover:bg-signup-button-hover"
+          >
+            Download
+          </button>
         </div>
-        <div className="mt-5">Applicant Name</div>
       </div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-        }}
-        className="block w-auto px-3 py-1 rounded-md bg-signup-button
-       text-white hover:bg-signup-button-hover"
-      >
-        Download application
-      </button>
     </div>
   );
 };
