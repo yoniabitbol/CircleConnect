f3c2211a8294685c11b87daff61e38df3ac38d9b
jonabitbol <tbnrsamurai@gmail.com>
2023-03-14 09:50:09 -0400

    removed duplicate posts in the feed

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -219,9 +219,11 @@ const getSocialFeed = async (req: Request, res: Response) => {
     const connectionsPosts = await Promise.all(
       currentUser?.connections.map((connectionID: string) => Post.find({ creatorID: connectionID, isJobListing: false }).populate('creator', 'name picture title')),
     );
+    const feedArray = currentUserPosts.concat(...connectionsPosts).sort((a: any, b: any) => b.createdAt - a.createdAt);
+    const feed = feedArray.filter((post: any, index:number) => feedArray.findIndex((p: any) => p._id.toString() === post._id.toString()) === index);
     return res.status(200).json({
       status: 'success',
-      data: currentUserPosts.concat(...connectionsPosts).sort((a: any, b: any) => b.createdAt - a.createdAt),
+      data: feed,
     });
   } catch (err) {
     return res.status(400).json({
@@ -238,9 +240,11 @@ const getJobFeed = async (req: Request, res: Response) => {
     const recruiterPosts = await Promise.all(
       currentUser?.preferenceTags.map((preferenceTag: string) => Post.find({ preferenceTags: { $in: [preferenceTag] }, isJobListing: true }).populate('creator', 'name picture title')),
     );
+    const feedArray = currentUserPosts.concat(...recruiterPosts).sort((a: any, b: any) => b.createdAt - a.createdAt);
+    const feed = feedArray.filter((post: any, index: number) => feedArray.findIndex((p: any) => p._id.toString() === post._id.toString()) === index);
     return res.status(200).json({
       status: 'success',
-      data: currentUserPosts.concat(...recruiterPosts).sort((a: any, b: any) => b.createdAt - a.createdAt),
+      data: feed,
     });
   } catch (err) {
     return res.status(400).json({
