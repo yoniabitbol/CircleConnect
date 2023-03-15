06e2cfbbb89ce1a9bb7aa5fd340e3fd82ef0ccc6
jonabitbol <tbnrsamurai@gmail.com>
2023-03-14 20:14:29 -0400

    added request to apply to posting

---
+++ client/src/http/applyToPost.ts
@@ -0,0 +1,28 @@
+import { auth } from "../firebase/config";
+const port = process.env.REACT_APP_BACKEND_PORT || 4000;
+
+async function applyToPost(post_id: string, formData: FormData) {
+    const currentUser = auth.currentUser;
+    const token = currentUser && (await currentUser.getIdToken());
+    const user_id = currentUser && currentUser.uid;
+    const url = `http://localhost:${port}/api/posts/${post_id}/apply`;
+
+    if(!user_id) {
+        return;
+    }
+    formData.append("applicantID", user_id);
+    formData.append("postID", post_id);
+
+    const res = await fetch(url, {
+        method: "PATCH",
+        headers: {Authorization: `Bearer ${token}`,},
+        body: formData,
+    });
+
+
+    if (!res.ok) {
+        throw new Error("Failed to apply to post.");
+    }
+}
+
+export default applyToPost;
