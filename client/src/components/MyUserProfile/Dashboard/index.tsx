ac1eb176d7df7dfb9471dd7a002ff348db1dc1d4
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-03-18 23:43:05 -0400

    wip(LC-148):Added i18n modules and changed links to buttons

--- client/src/components/MyUserProfile/Dashboard/index.tsx
+++ client/src/components/MyUserProfile/Dashboard/index.tsx
@@ -1,7 +1,10 @@
 import JobPosting from "./JobPosting";
 import JobApplied from "./JobApplied";
+import { useTranslation } from "react-i18next";
+import React from "react";
 
 const Dashboard: React.FC = () => {
+  const {t} = useTranslation();
   return (
     <div>
       <div className="w-full p-5 rounded-md bg-slate-200 mx-auto">
@@ -11,7 +14,7 @@ const Dashboard: React.FC = () => {
         <JobPosting />
       </div>
       <div className="mt-2 w-full p-5 rounded-md bg-slate-200 mx-auto">
-        Jobs you applied to:
+        {t('jobApplied.label.appliedTo')} 
         <JobApplied />
         <JobApplied />
         <JobApplied />
---
+++ client/src/i18n/locales/fr/common.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const common = {
+    common: {
+        label: {
+            motto:'Connectez-vous, collaborez et développez votre cercle'    
+        },
+        buttons: {
+            login: 'Connexion',
+            signup: 'Inscrivez-vous ici',
+            logout:'Déconnexion'
+        }
+    }
+}
+export default common;
