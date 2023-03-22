f09df5d107ae45d1b6aca086b4c0dec9a62dbb82
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-03-21 23:50:36 -0400

    wip(LC-148): added user profile i18"

---
+++ client/src/i18n/locales/fr/jobPosted.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+const jobPosted = {
+
+    jobPosted: {
+        label: {
+            applicants:'0 candidats',
+            companyName:'Nom de l\'entreprise',
+            location:'Emplacement',
+            posted:"Offres d'emploi que vous avez publiées :",
+            applicantList:'Candidats:',
+
+        },
+        buttons: {
+            jobTitle:'Titre du poste',
+            viewApplication: 'Voir la demande',
+            delete:'Supprimer',
+            allApplicants: 'Voir tous les candidats',
+            close: 'fermer'
+        }
+    }
+}
+export default jobPosted;
