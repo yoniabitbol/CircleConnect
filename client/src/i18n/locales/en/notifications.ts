1672814ca9cc76f8449df02ed66680b0fe5afa0d
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-03-27 01:54:36 -0400

    WIP(LC-148):Added i18n for the notifications page and Added some fixes for the profile

---
+++ client/src/i18n/locales/en/notifications.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const notifications = {
+    notifications: {
+        label: {
+            onYourMind:'À quoi pensez-vous ?',
+            notifSettings:'NOTIFICATION SETTINGS',
+            recent:'RECENT',
+            notifications:'NOTIFICATIONS',
+            dashboard:'YOUR DASHBOARD',
+            stats:'GO TO STATS',
+            viewsToday:'views today',
+            postViews:'posts views',
+            searchAppearance:'search appearances'
+        },
+        buttons: {
+            newPost:'New Post',
+        }
+    }
+}
+export default notifications;
