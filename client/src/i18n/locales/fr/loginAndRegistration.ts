e1c9d28faf67d66750d5aa827db931ef8cc59358
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-03-27 01:54:36 -0400

    WIP(LC-148):Added i18n for the notifications page and Added some fixes for the profile

---
+++ client/src/i18n/locales/fr/loginAndRegistration.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+const loginAndRegistration = {
+    loginAndRegistration: {
+        label: {
+            continueWith:'Ou continuer avec',
+            noAccount:"Si vous n'avez pas encore de compte",
+            haveAccount:'Si vous avez déjà un compte',
+            youCan:'Vous pouvez',
+            invalidPassword:'Mot de passe invalide',
+            signup:'Inscrivez-vous',
+            login:'Connexion',
+            emailRequired:"L'adresse électronique est obligatoire",
+            invalidEmail:'Adresse électronique invalide',
+            emailRegistered:'Email déjà enregistré',
+            emailNotRegistered:"L'adresse électronique n'est pas enregistrée",
+            passwordRequired:'Le mot de passe est requis',
+            passwordLength:'Le mot de passe doit comporter au moins 8 caractères',
+            nameIsRequired:'Le nom est obligatoire',
+            nameLength:'Le nom doit contenir au moins 3 caractères',
+            email:'Courriel',
+            password:'Mot de passe',
+            firstName:'Prénom',
+            lastName:'Nom de famille',
+        },
+        buttons: {
+            login: 'Connexion',
+            signup: 'vous inscrire ici',
+            register:'Inscription',
+            logout:'Déconnexion',
+            forgotPassword:'Mot de passe oublié ?',
+        }
+    }
+}
+export default loginAndRegistration;
