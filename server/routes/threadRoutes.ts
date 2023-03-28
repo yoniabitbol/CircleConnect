dc48d70f1eac0eb67bd17527be3ec89e81108e56
jonabitbol <tbnrsamurai@gmail.com>
2023-03-28 15:21:31 -0400

    fixed thread and message controllers

--- server/routes/threadRoutes.ts
+++ server/routes/threadRoutes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import threadController from '../controllers/threadController';
+import messageController from '../controllers/messageController';
 
 const router = express.Router();
 
 router.route('/')
-  .post(threadController.getUserThreads);
+  .get(threadController.getAllThreads)
+  .post(threadController.createThread);
 
 router.route('/:user_id')
   .get(threadController.getUserThreads);
+
+router.route('/:thread_id/messages')
+  .get(messageController.getThreadMessages)
+  .post(messageController.createMessage);
 
 export default router;
