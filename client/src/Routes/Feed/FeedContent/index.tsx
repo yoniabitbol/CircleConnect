8d2ee3192e2f3ac1e1aaa96e23af6d4eb1737a8b
Reuven1203 <reuven1203@gmail.com>
2023-03-31 02:49:18 -0400

    Update index.tsx
    This reverts commit 4a5cabd08111af2f92219b8c81e030d8267158c2.

--- client/src/Routes/Feed/FeedContent/index.tsx
+++ client/src/Routes/Feed/FeedContent/index.tsx
@@ -1,24 +1,6 @@
 import {FC, useEffect, useRef, useState} from 'react';
 import FeedCard from '../FeedCard';
 
-const feedDataD = [{
-    _id:1,
-    creator: {
-        name: 'John Doe',
-        title: 'Software Engineer',
-        img:''
-    },
-        text: 'looking for applicants',
-        img: 'https://media.istockphoto.com/id/1364176524/vector/now-hiring-random-shapes-blue-business-symbol' +
-            's-background.jpg?s=612x612&w=0&k=20&c=Gna-i8qknM9oDuzNx-7WSyaqPbNz5yc7ne4j3F05ILQ=',
-        comments: ['comment1', 'comment2'],
-        likes: ['like1', 'like2'],
-        isJobListing: true,
-        isResumeRequired: true,
-        isCoverLetterRequired: true,
-},
-
-]
 
 
 const FeedContent:FC<{ feedData: any}> = (props) => {
@@ -33,7 +15,7 @@ const FeedContent:FC<{ feedData: any}> = (props) => {
     console.log(feedData)
     return (
         <div className="flex-row w-full justify-center">
-            {feedDataD && feedDataD.map((data : any, i: number) => {
+            {feedData && feedData.map((data : any, i: number) => {
                 return ( <div key={data.id} ref={i === scrollTo ? ref: null}>
                     <FeedCard  userInfo={data.creator} scrollTo={setScrollTo.bind(this, i)}
                               postInfo={{id:data._id,text: data.text, img: data.image, comments: data.comments, date: data.createdAt, position: data.position}}
