0eba75d74c06e90afdbdeda10097f04f541a49b7
jonabitbol <tbnrsamurai@gmail.com>
2023-04-09 17:13:11 -0400

    added sendNotification http request

---
+++ client/src/http/sendNotification.ts
@@ -0,0 +1,29 @@
+import { auth } from "../firebase/config";
+const host = process.env.REACT_APP_HOST || 'localhost';
+const port = process.env.REACT_APP_BACKEND_PORT || 4000;
+
+async function sendNotification (target_user_id: string, type: string, initiator_id: string) {
+    const currentUser = auth.currentUser;
+    const token = currentUser && (await currentUser.getIdToken());
+    const url = `http://${host}:${port}/api/notifications/${target_user_id}`;
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            type,
+            initiator_id,
+        }),
+        });
+
+    if (!res.ok) {
+        throw new Error("Failed to send notification.");
+    }
+
+    return res.json();
+}
+
+export default sendNotification;
