03a5ddcc85c2055171e310ec74eebffc8b68cf81
Ankita Ingle <aningle31@gmail.com>
2023-04-09 10:33:41 -0400

    Get cover letter

--- client/src/components/MyUserProfile/Dashboard/JobPosting/DownloadCV/index.tsx
+++ client/src/components/MyUserProfile/Dashboard/JobPosting/DownloadCV/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { applicationType } from "../../../../../Models/UserProfileModel";
 import { Usertypes } from "../../../../UserProfile";
 
@@ -7,28 +8,29 @@ interface DownloadCVProps {
 }
 
 const DownloadCV: React.FC<DownloadCVProps> = ({ applicant, postID }) => {
+  const [fileName, setFileName] = useState("");
   // console.log(applicant);
   // console.log(postID);
   return (
     <div>
       <div>
         <div className="">
-          <button
+          <a
+            href={fileName}
+            download
             onClick={(e) => {
               e.preventDefault();
               applicant.applications.map((application: applicationType) => {
                 if (application.postID === postID) {
-                  console.log(application.coverLetter);
+                  setFileName(application.coverLetter);
                 }
               });
             }}
             className="text-xs block w-auto px-3 py-1 rounded-md bg-signup-button
           text-white hover:bg-signup-button-hover"
           >
-            <a href="" download>
-              Download
-            </a>
-          </button>
+            Download
+          </a>
         </div>
       </div>
     </div>
