56208a11b48906691437cd7f6fab4eb1dd63a556
Ankita Ingle <aningle31@gmail.com>
2023-04-10 09:30:32 -0400

    Add no jobs posted / applied message

--- client/src/components/MyUserProfile/Dashboard/index.tsx
+++ client/src/components/MyUserProfile/Dashboard/index.tsx
@@ -13,19 +13,27 @@ const Dashboard: React.FC<DashboardProps> = ({ posts, applications }) => {
     <div>
       <div className="w-full p-5 rounded-md bg-slate-200 mx-auto">
         Jobs you posted:
-        {posts.map((post: postType) => (
-          <div key={post._id}>
-            {post.isJobListing ? <JobPosting post={post} /> : null}
-          </div>
-        ))}
+        {posts.length == 0 ? (
+          <div className="text-sm">You have not posted any jobs yet...</div>
+        ) : (
+          posts.map((post: postType) => (
+            <div key={post._id}>
+              {post.isJobListing ? <JobPosting post={post} /> : null}
+            </div>
+          ))
+        )}
       </div>
       <div className="mt-2 w-full p-5 rounded-md bg-slate-200 mx-auto">
         Jobs you applied to:
-        {applications.map((application: applicationType) => (
-          <div key={application._id}>
-            <JobApplied application={application} />
-          </div>
-        ))}
+        {applications.length == 0 ? (
+          <div className="text-sm">You have not applied to any jobs yet...</div>
+        ) : (
+          applications.map((application: applicationType) => (
+            <div key={application._id}>
+              <JobApplied application={application} />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
