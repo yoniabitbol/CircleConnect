7911870419d8952f7b14f06d8a363d5e685f54a8
jonabitbol <tbnrsamurai@gmail.com>
2023-04-09 22:58:52 -0400

    code cleanup

--- client/src/http/sendNotification.ts
+++ client/src/http/sendNotification.ts
@@ -2,7 +2,7 @@ import { auth } from "../firebase/config";
 const host = process.env.REACT_APP_HOST || 'localhost';
 const port = process.env.REACT_APP_BACKEND_PORT || 4000;
 
-async function sendNotification (target_user_id: string, type: string, initiator_id: string) {
+async function sendNotification (target_user_id: string,  initiator_id: string, type: string) {
     const currentUser = auth.currentUser;
     const token = currentUser && (await currentUser.getIdToken());
     const url = `http://${host}:${port}/api/notifications/${target_user_id}`;
