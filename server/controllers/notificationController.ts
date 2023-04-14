f2e2f9d8ef7fc42d5549a986775995a491bca81a
Ankita Ingle <aningle31@gmail.com>
2023-04-14 16:16:09 -0400

    Add check for req.body

--- server/controllers/notificationController.ts
+++ server/controllers/notificationController.ts
@@ -2,75 +2,80 @@ import { Request, Response } from 'express';
 import Notification from '../models/notificationModel';
 
 const getUnreadNotifications = async (req: Request, res: Response) => {
-  try {
-    const notifications = await Notification.find({
-      user_id: req.params.user_id,
-      isRead: false,
-    }).populate('initiator', 'name picture');
-    res.status(200).json({
-      status: 'success',
-      data: {
-        notifications,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: `ERROR ${err}`,
-      message: 'Failed to get unread notifications',
-    });
-  }
+	try {
+		const notifications = await Notification.find({
+			user_id: req.params.user_id,
+			isRead: false,
+		}).populate('initiator', 'name picture');
+		res.status(200).json({
+			status: 'success',
+			data: {
+				notifications,
+			},
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: `ERROR ${err}`,
+			message: 'Failed to get unread notifications',
+		});
+	}
 };
 
 const sendNotification = async (req: Request, res: Response) => {
-  try {
-    if (req.body.type !== 'message' || req.body.type !== 'connection' || req.body.type !== 'relatedPost') {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid notification type',
-      });
-    }
+	console.log(req.body);
+	try {
+		if (
+			req.body.type !== 'message' ||
+			req.body.type !== 'connection' ||
+			req.body.type !== 'relatedPost'
+		) {
+			return res.status(400).json({
+				status: 'failure',
+				message: 'Invalid notification type',
+			});
+		}
 
-    const notification = await Notification.create({
-      user_id: req.params.user_id,
-      type: req.body.type,
-      initiatorID: req.body.initiatorID,
-    });
-    return res.status(201).json({
-      status: 'success',
-      data: {
-        notification,
-      },
-    });
-  } catch (err) {
-    return res.status(400).json({
-      status: `ERROR ${err}`,
-      message: 'Failed to send notification',
-    });
-  }
+		const notification = await Notification.create({
+			user_id: req.params.user_id,
+			type: req.body.type,
+			initiatorID: req.body.initiatorID,
+		});
+		return res.status(201).json({
+			status: 'success',
+			data: {
+				notification,
+			},
+		});
+	} catch (err) {
+		return res.status(400).json({
+			status: `ERROR ${err}`,
+			message: 'Failed to send notification',
+		});
+	}
 };
 
 const markNotificationAsRead = async (req: Request, res: Response) => {
-  try {
-    const notification = await Notification.updateMany(
-      { user_id: req.params.user_id },
-      { isRead: true },
-    );
-    res.status(200).json({
-      status: 'success',
-      data: {
-        notification,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: `ERROR ${err}`,
-      message: 'Failed to mark notification as read',
-    });
-  }
+	try {
+		const notification = await Notification.updateMany(
+			{ user_id: req.params.user_id },
+			{ isRead: true }
+		);
+		res.status(200).json({
+			status: 'success',
+			data: {
+				notification,
+			},
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: `ERROR ${err}`,
+			message: 'Failed to mark notification as read',
+		});
+	}
 };
 
 export default {
-  getUnreadNotifications,
-  sendNotification,
-  markNotificationAsRead,
+	getUnreadNotifications,
+	sendNotification,
+	markNotificationAsRead,
 };
