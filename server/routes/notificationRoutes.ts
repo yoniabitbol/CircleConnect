7cc1d20db3740642084b34ba7cdde41f3c00c620
jonabitbol <tbnrsamurai@gmail.com>
2023-04-16 17:26:37 -0400

    add new request for all of a user's notifications

--- server/routes/notificationRoutes.ts
+++ server/routes/notificationRoutes.ts
@@ -5,8 +5,11 @@ const router = express.Router();
 
 // Notification API routes
 router.route('/:user_id')
-  .get(notificationController.getUnreadNotifications)
+  .get(notificationController.getUserNotifications)
   .post(notificationController.sendNotification)
   .patch(notificationController.markNotificationAsRead);
+
+router.route('/:user_id/unread')
+  .get(notificationController.getUnreadNotifications);
 
 export default router;
