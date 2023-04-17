a1884d31aff1f69412bd060d0718bb0d05327c92
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-04-17 02:37:13 -0400

    WIP(LC-43):added the package and testing string for a proof of concept

--- client/src/components/Chat/Message/index.tsx
+++ client/src/components/Chat/Message/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-
+import Filter from "badwords-filter";
 const Message: React.FC<{
   outbound: boolean;
   text: string;
 }> = ({ outbound, text }) => {
   const textBoxStyle = outbound ? "bg-slate-100" : "bg-indigo-700 text-sky-50";
-
+  console.log(`Testt ${text}`);
+  const filter = new Filter({ list: ["badword","test","bob"] });
+  console.log(filter.clean(text))
   return (
     <div>
       {outbound ? <span className="" /> : <span />}
 
       <div className={"rounded-md inline-block px-3 py-1 " + textBoxStyle}>
-        <span>{text}</span>
+        <span>{filter.clean(text)}</span>
       </div>
     </div>
   );
