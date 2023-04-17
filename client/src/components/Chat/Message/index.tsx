ab83523aa8340607dcb5485e586da83fdf62a96d
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2023-04-17 12:23:13 -0400

    WIP(LC-43):Added a filter message component

--- client/src/components/Chat/Message/index.tsx
+++ client/src/components/Chat/Message/index.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import Filter from "badwords-filter";
+import FilterMessage from "./filterMessages";
 const Message: React.FC<{
   outbound: boolean;
   text: string;
 }> = ({ outbound, text }) => {
   const textBoxStyle = outbound ? "bg-slate-100" : "bg-indigo-700 text-sky-50";
-  console.log(`Testt ${text}`);
-  const filter = new Filter({ list: ["badword","test","bob"] });
-  console.log(filter.clean(text))
   return (
     <div>
       {outbound ? <span className="" /> : <span />}
 
       <div className={"rounded-md inline-block px-3 py-1 " + textBoxStyle}>
-        <span>{filter.clean(text)}</span>
+        <FilterMessage text={text} />
       </div>
     </div>
   );
