78439603d1599407cde6d6cf7b6078138ed7ed2f
Ankita Ingle <aningle31@gmail.com>
2023-04-17 10:14:48 -0400

    Fix naming of req

--- client/src/http/getUserNotifications.ts
+++ client/src/http/getUserNotifications.ts
@@ -1,26 +1,26 @@
 import { auth } from "../firebase/config";
-const host = process.env.REACT_APP_HOST || 'localhost';
+const host = process.env.REACT_APP_HOST || "localhost";
 const port = process.env.REACT_APP_BACKEND_PORT || 4000;
 
-async function getUnreadNotification () {
-    const currentUser = auth.currentUser;
-    const token = currentUser && (await currentUser.getIdToken());
-    const currentUserId = currentUser && currentUser.uid;
-    const url = `http://${host}:${port}/api/notifications/${currentUserId}`;
+async function getUserNotifications() {
+  const currentUser = auth.currentUser;
+  const token = currentUser && (await currentUser.getIdToken());
+  const currentUserId = currentUser && currentUser.uid;
+  const url = `http://${host}:${port}/api/notifications/${currentUserId}`;
 
-    const res = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-    });
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    if (!res.ok) {
-        throw new Error("Failed to get user notifications.");
-    }
+  if (!res.ok) {
+    throw new Error("Failed to get user notifications.");
+  }
 
-    return res.json();
+  return res.json();
 }
 
-export default getUnreadNotification;
+export default getUserNotifications;
