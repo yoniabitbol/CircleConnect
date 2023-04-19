5bca8c859ebe50dba199966e32c436d2784e6b83
Ankita Ingle <aningle31@gmail.com>
2023-04-19 16:03:57 -0400

    Fix empty notification bug

--- client/src/components/UserNotifications/ConnectionInvite/index.tsx
+++ client/src/components/UserNotifications/ConnectionInvite/index.tsx
@@ -14,7 +14,6 @@ const ConnectionInvite: React.FC<{
   const [picture, setPicture] = useState<any>();
   const [connectionState, setConnectionState] = useState("unset");
 
-
   useEffect(() => {
     async function fetchUser(user_id: string) {
       try {
@@ -42,64 +41,70 @@ const ConnectionInvite: React.FC<{
   }, []);
 
   return (
-    <div className="w-full m-4 py-3 rounded-md bg-white h-auto dark:secondary-dark">
-      <div className="flex flex-row justify-between px-3">
-        <div className="flex flex-row space-x-8 ">
-          <div className="pt-2">
-            <Avatar sx={{ width: 50, height: 50 }} src={picture} />
-          </div>
-          <div className="flex flex-col">
-            <h1 className="text-md font-bold">{user?.name}</h1>
-            <h2 className="text-xs font-semibold">{user?.title}</h2>
-            <h3 className="text-xs text-[#4B47B7] dark:main-color">
-              {user?.connections.length} connections
-            </h3>
-          </div>
-        </div>
-        <div className="hidden md:flex flex-row w-1/3">
-          <h1 className="text-4xl pr-2" style={{ color: "#4B47B7" }}>
-            |
-          </h1>
-          <h3 className="text-sm text-gray-400">{"Hey! Let's connect..."}</h3>
-        </div>
-        <div className="flex flex-row space-x-1 lg:space-x-4 mt-4">
-          {connectionState == "unset" ? (
+    <div>
+      {user == null ? null : (
+        <div className="w-full m-4 py-3 rounded-md bg-white h-auto dark:secondary-dark">
+          <div className="flex flex-row justify-between px-3">
+            <div className="flex flex-row space-x-8 ">
+              <div className="pt-2">
+                <Avatar sx={{ width: 50, height: 50 }} src={picture} />
+              </div>
+              <div className="flex flex-col">
+                <h1 className="text-md font-bold">{user?.name}</h1>
+                <h2 className="text-xs font-semibold">{user?.title}</h2>
+                <h3 className="text-xs text-[#4B47B7] dark:main-color">
+                  {user?.connections.length} connections
+                </h3>
+              </div>
+            </div>
+            <div className="hidden md:flex flex-row w-1/3">
+              <h1 className="text-4xl pr-2" style={{ color: "#4B47B7" }}>
+                |
+              </h1>
+              <h3 className="text-sm text-gray-400">
+                {"Hey! Let's connect..."}
+              </h3>
+            </div>
             <div className="flex flex-row space-x-1 lg:space-x-4 mt-4">
-              <button
-                type="submit"
-                className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md text-sm w-24 h-8"
-                onClick={() => {
-                  acceptConnectionRequest(initiatorID);
-                  setConnectionState("connected");
-                }}
-              >
-                ACCEPT
-              </button>
-              <button
-                type="submit"
-                className="bg-slate-400 hover:bg-slate-500 text-white rounded-md text-sm w-24 h-8"
-                onClick={() => {
-                  declineConnectionRequest(initiatorID);
+              {connectionState == "unset" ? (
+                <div className="flex flex-row space-x-1 lg:space-x-4 mt-4">
+                  <button
+                    type="submit"
+                    className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md text-sm w-24 h-8"
+                    onClick={() => {
+                      acceptConnectionRequest(initiatorID);
+                      setConnectionState("connected");
+                    }}
+                  >
+                    ACCEPT
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-slate-400 hover:bg-slate-500 text-white rounded-md text-sm w-24 h-8"
+                    onClick={() => {
+                      declineConnectionRequest(initiatorID);
 
-                  setConnectionState("notConnected");
-                }}
-              >
-                DECLINE
-              </button>
+                      setConnectionState("notConnected");
+                    }}
+                  >
+                    DECLINE
+                  </button>
+                </div>
+              ) : null}
+              {connectionState === "connected" ? (
+                <div className="text-xs text-b">
+                  {user?.name + " is now a connection."}
+                </div>
+              ) : null}
+              {connectionState === "notConnected" ? (
+                <div className="text-xs text-b">
+                  {"Connection request declined."}
+                </div>
+              ) : null}
             </div>
-          ) : null}
-          {connectionState === "connected" ? (
-            <div className="text-xs text-b">
-              {user?.name + " is now a connection."}
-            </div>
-          ) : null}
-          {connectionState === "notConnected" ? (
-            <div className="text-xs text-b">
-              {"Connection request declined."}
-            </div>
-          ) : null}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
